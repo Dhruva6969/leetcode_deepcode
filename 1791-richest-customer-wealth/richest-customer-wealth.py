c30e32f4935513f0a16624bec43248be54a21bc0
Dhruva6969 <dhruvsharma8716@gmail.com>
2026-04-04 21:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (96.22%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type accounts: List[List[int]]
         :rtype: int
         """
+        # richest=0
+        # for customer in accounts:
+        #     wealth=0
+        #     for bank in customer:
+        #         wealth+=bank
+        #     richest=max(richest,wealth)
+        # return richest
+
+        wealth=0
         richest=0
         for customer in accounts:
-            wealth=0
-            for bank in customer:
-                wealth+=bank
+            
+            wealth=sum(customer)
             richest=max(richest,wealth)
         return richest
