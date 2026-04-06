b34c8001e88266e35dd340d2e5a34e6715ea4e47
Dhruva6969 <dhruvsharma8716@gmail.com>
2026-04-06 15:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.62%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        arr=[]
+        while head:
+            arr.append(head)
+            head=head.next
+        return arr[len(arr)//2]
