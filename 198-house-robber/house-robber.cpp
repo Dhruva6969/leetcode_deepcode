7fa1cdfc56dd11e12b07ecf2c90b3ebb487c08a7
Dhruv Sharma <dhruvsharma8716@gmail.com>
2026-09-14 22:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (23.60%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n= nums.size();
+    int n= nums.size();
 
-        if(n==1)    return nums[0];
+    if(n==1)    return nums[0];
 
         vector<int> dp(n+1, -1);
 
